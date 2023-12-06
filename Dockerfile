b1ecebefe107b1e1d418163464750c013c68540e
“najikh2002” <hizbullahnajihan@gmail.com>
2023-12-07 00:55:23 +0700

    test

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.12.0
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from types import List
+
+app = FastAPI()
+db = []
+
+
+
+
