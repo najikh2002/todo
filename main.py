817496021a4546989f51c8c76f6f2c54bbf2e2b4
“najikh2002” <hizbullahnajihan@gmail.com>
2023-12-07 01:41:14 +0700

    feat: read & update method request

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from types import List
 
@@ -19,4 +19,24 @@ class PostProps(BaseModel):
 @app.post("/post", response_model=List(PostProps))
 def create_post(item):
     db.append(item)
+
     return item
+
+@app.get("/post/{post_id}", response_model= PostProps)
+def read_post(post_id: int):
+    if post_id < 0 or post_id > len(db):
+        raise HTTPException(status_code=404, detail="TODO list is not found")
+    
+    return db[post_id]
+
+@app.put("/post/{post_id}", response_model= PostProps)
+def update_post(post_id: int, updated_item: PostProps):
+    if post_id < 0 or post_id > len(db):
+        raise HTTPException(status_code=404, detail="TODO list is not found")
+    
+    db[post_id] = updated_item
+
+    return db[post_id]
+
+
+
